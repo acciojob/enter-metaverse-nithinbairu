f95c15b94ab290389a0fe6520d695413590f3235
nithinbairu <113778598+nithinbairu@users.noreply.github.com>
2025-09-29 10:55:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-//your JS code here. If required.
-document.getElementById("enterBtn").addEventListener("click",()=>{
-	pTag=document.getElementById("status");
-	h1Tag=document.createElement("h1");
-	h1Tag.id=pTag.id;
-	h1Tag.innerText="Entered Metaverse";
-	pTag.parentNode.replaceChild(h1Tag,pTag);
-})
+window.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("enterBtn").addEventListener("click", () => {
+    let pTag = document.getElementById("status");
+    let h1Tag = document.createElement("h1");
+    h1Tag.id = pTag.id;
+    h1Tag.innerText = "Entered Metaverse";
+    pTag.parentNode.replaceChild(h1Tag, pTag);
+  });
+});
