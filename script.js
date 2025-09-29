ecfc46f8a7520da6f53c2a2d33a35304bb3c5502
nithinbairu <113778598+nithinbairu@users.noreply.github.com>
2025-09-29 10:32:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+document.getElementById("enterBtn").addEventListener("click",()=>{
+	pTag=document.getElementById("status");
+	h1Tag=document.createElement("h1");
+	h1Tag.id=pTag.id;
+	h1Tag.innerText="Entered Metaverse";
+	pTag.parentNode.replaceChild(h1Tag,pTag);
+})
